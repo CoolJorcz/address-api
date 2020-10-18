37043660bf0c7c542aa439ccecf1f4147da663fc
Andrew Jorczak <andrew.jorczak@gmail.com>
2020-10-17 18:54:55 -0700

    Adding in state and zip validations.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,7 @@
 class Address < ApplicationRecord
-  validates :state, length: { is: 2 }
+  validates :line1, presence: true
+  validates :city, presence: true
+  validates :state, length: { is: 2 }, presence: true
+  validates :zip, presence: true
   validates_format_of :zip, with: /\d{5}(?:[-\s]\d{4})?/
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 describe Address, type: :model do
   describe 'validations' do
+    it { is_expected.to validate_presence_of(:line1) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:zip) }
+    
     describe '#state' do
       it { is_expected.to validate_length_of(:state).is_equal_to(2) }
     end
