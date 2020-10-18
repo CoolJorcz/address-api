800f10e18144db47c19d79a6548fc68c9341d4fd
Andrew Jorczak <andrew.jorczak@gmail.com>
2020-10-17 18:50:41 -0700

    Validations on state and zip.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,4 @@
 class Address < ApplicationRecord
+  validates :state, length: { is: 2 }
+  validates_format_of :zip, with: /\d{5}(?:[-\s]\d{4})?/
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 describe Address, type: :model do
   describe 'validations' do
-    it  { is_expected.to validate_length_of(:state).is_equal_to(2) }
+    describe '#state' do
+      it { is_expected.to validate_length_of(:state).is_equal_to(2) }
+    end
     
     describe '#zip' do
       it { is_expected.to allow_value('90201').for(:zip) }
       it { is_expected.to allow_value('01047').for(:zip) }
+      it { is_expected.to allow_value('90201-4566').for(:zip) }
       it { is_expected.not_to allow_value('notazip!').for(:zip) }
     end
   end
